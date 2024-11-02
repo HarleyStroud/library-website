014326f4432dc48623885fbd4bc0683f142f34ef
HarleyStroud <stroudhd@gmail.com>
2024-11-02 08:50:03 -0400

    Add checkbox to book entries to allow the books read status to be changed

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const newBookButton = document.querySelector("#new_book_button");
 const bookDisplay = document.querySelector("#book_display");
 const newBookForm = document.querySelector("#new_book_form");
 const newBookDialog = document.querySelector('#new_book_dialog');
+const readButton = document.querySelector("#read");
 
 const myLibrary = [];
 
@@ -13,6 +14,10 @@ function Book(title, author, pages, read) {
     this.author = author;
     this.pages = pages;
     this.read = read;
+
+    this.toggleRead = function() {
+        this.read = !read;
+    }
 
     this.info = function() {
         let readStatusText = "";
@@ -27,37 +32,59 @@ function Book(title, author, pages, read) {
 }
 
 
-function addBookToLibrary(book) {
-    myLibrary.push(book);
-    newBookDialog.close();
-    displayLibrary();
+function displayLibrary() {
+    for(let i = 0; i < myLibrary.length; i++) {
+        addNewBookToDisplay(myLibrary[i]);
+    }
 }
 
 
-function displayLibrary() {
-    for(let i = 0; i < myLibrary.length; i++) {
-        // crate new book entry display
-        const newBookEntry = document.createElement("div");
-        newBookEntry.classList.add("book_entry");
+function addBookToLibrary(book) {
+    myLibrary.push(book);
+    newBookDialog.close();
+    addNewBookToDisplay(book);
+}
 
-        const titleDiv = document.createElement("div");
-        titleDiv.textContent = myLibrary[i].title;
 
-        const authroDiv = document.createElement("div");
-        authroDiv.textContent = myLibrary[i].author;
+function addNewBookToDisplay(book) {
+    // crate new book entry display
+    const newBookEntry = document.createElement("div");
+    newBookEntry.classList.add("book_entry");
 
-        const pagesDiv = document.createElement("div");
-        pagesDiv.textContent = myLibrary[i].pages;
+    const titleDiv = document.createElement("div");
+    titleDiv.textContent = `Title: ${book.title}`;
 
-        const readDiv = document.createElement("div");
-        readDiv.textContent = myLibrary[i].read;
+    const authroDiv = document.createElement("div");
+    authroDiv.textContent = `Author: ${book.author}`;
 
-        newBookEntry.appendChild(titleDiv);
-        newBookEntry.appendChild(authroDiv);
-        newBookEntry.appendChild(pagesDiv);
-        newBookEntry.appendChild(readDiv);
-        bookDisplay.appendChild(newBookEntry);
-    }
+    const pagesDiv = document.createElement("div");
+    pagesDiv.textContent = `Pages: ${book.pages}`;
+
+    const readCheckbox = document.createElement("input");
+    readCheckbox.type = "checkbox";
+    readCheckbox.id = "readButton";
+    readCheckbox.checked = book.read;
+
+    // Create read checkbox with a label.
+    const readCheckboxWrapper = document.createElement("p");
+    const readCheckBoxLabel = document.createElement("label");
+    readCheckBoxLabel.setAttribute("for", "#readButton");
+    readCheckBoxLabel.textContent = "Read";
+
+    readCheckboxWrapper.appendChild(readCheckBoxLabel);
+    readCheckboxWrapper.appendChild(readCheckbox);
+
+    readCheckbox.addEventListener("change", () => {
+        book.read = readCheckbox.checked;
+        console.log(book);
+    });
+
+
+    newBookEntry.appendChild(titleDiv);
+    newBookEntry.appendChild(authroDiv);
+    newBookEntry.appendChild(pagesDiv);
+    newBookEntry.appendChild(readCheckboxWrapper);
+    bookDisplay.appendChild(newBookEntry);
 }
 
 
@@ -70,14 +97,10 @@ newBookForm.addEventListener('submit', (event) => {
     const bookTitle = document.querySelector("#book_title").value;
     const author = document.querySelector("#author").value;
     const pages = document.querySelector("#pages").value;
-    const read = document.querySelector("#read").value;
-    const newBook = new Book(bookTitle, author, pages, read);
+    const isBookRead = readButton.checked;
+    const newBook = new Book(bookTitle, author, pages, isBookRead);
+
     addBookToLibrary(newBook);
-
     event.preventDefault();
-    console.log(bookTitle);
+    console.log(newBook);
 });
-
-
-const myBook = new Book("My Book", "Harley Stroud", 543, false);
-console.log(myBook.info());
